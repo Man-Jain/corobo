44e15d040d6b0eeabc663303df5a6cc03d091fbf
Manank Patni <manank321@gmail.com>
2018-10-14 17:16:32 +0530

    ci: Add Pull Request check to semaphore scripts
    This fixes the condition of build failure when someone creates a
    pull request from the master branch. It now check that the project
    is coala repo and then execute build steps
    
    Fixes https://github.com/coala/corobo/issues/583

--- .ci/semaphore.answers.sh
+++ .ci/semaphore.answers.sh
@@ -5,7 +5,7 @@ pull_and_build() {
     docker build -t meetmangukiya/corobo-answers answers/
 }
 
-if [[ $BRANCH_NAME != "master" ]]
+if [[ $BRANCH_NAME != "master" && $SEMAPHORE_REPO_SLUG != "coala/corobo" ]]
 then
     .ci/check_docker.py
     if [[ $? == 1 ]]
